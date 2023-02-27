c019e917523345177dd056e8e228397638775969
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-02-27 15:19:17 +0100

    create student service and controller

--- src/main/java/com/tuto/demo/DemoApplication.java
+++ src/main/java/com/tuto/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.tuto.demo;
 
+import com.tuto.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public List<Student> hello(
+	) {
+		return List.of(
+				new Student(
+						1L ,
+						"ibrahim" ,
+						"ibrahim@gmail.com",
+						LocalDate.of(2000,04 , 25),
+						22
+
+				)
+		);
 	}
 
 }
---
+++ src/main/java/com/tuto/demo/student/StudentService.java
@@ -0,0 +1,26 @@
+package com.tuto.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents(
+    ) {
+
+        return List.of(
+                new Student(
+                        1L ,
+                        "ibrahim" ,
+                        "ibrahim@gmail.com",
+                        LocalDate.of(2000,04 , 25),
+                        22
+
+                )
+        );
+    }
+}
